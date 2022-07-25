6948051a30c0ed43c8ff3bfbc603b08a0b396463
mohdadil27 <mohdadil2721@gmail.com>
2022-07-25 18:14:09 +0530

    yreactwebsite

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import './App.css';
+import Banner from './components/Banner';
+import Slider from './components/Slider';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+   <Header />
+   <Banner />
+   <Slider />
+   <Footer />
+</>
+  );
+}
+
+export default App;
